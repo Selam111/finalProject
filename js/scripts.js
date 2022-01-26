4b437ec215d3ce426a8a8e3f3e7b11e00112e49a
Selam Tefera <selamwa@gmail.com>
2022-01-26 12:16:45 -0500

    First Trial

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,27 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+// window.addEventListener('DOMContentLoaded', event => {
+//     const listHoursArray = document.body.querySelectorAll('.list-hours li');
+//     listHoursArray[new Date().getDay()].classList.add(('today'));
+// })
+const buttonElement = document.getElementById('secret-button');
+const imgElement = document.getElementById('secret-img');
+ 
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+ 
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(imgElement);
+});
+const payment1 = "file:///D:/HTMLandCSS/FinalProjectTeam6/checkOutForm.html";
+let Addcart = document.getElementById('cart-1');
+Addcart.onclick = () => {
+  Addcart.src = payment1;
+
+}
